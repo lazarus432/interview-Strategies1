16da554d7f8f88efd5f89afd3b273821002c0c88
Duex <deux02030@gmail.com>
2017-10-19 01:15:51 -0400

    docs: updated q3 to check user input
    Added a while loop to check if user enters input.

--- q3.py
+++ q3.py
@@ -2,18 +2,21 @@
 user_list = []
 # takes 5 inputs from a user and stores them into the user_list array 
 for i in range(0,5):
-	list = str(raw_input('Please enter a list item: '))
-	user_list.append(list)
+    list = str(raw_input('Please enter a list item: '))
+    # check if user entered a list item
+    while not list:
+        list = str(raw_input("You didn't input anything. Please enter a list item: "))
+    user_list.append(list)
 
 
 def list(strings):
-	# create variable to hold the list					
-	string = "<ul><br>"
-	# loop through the list of strings and apply list tag to each element. 				
-	for s in strings:
-		string += "<li>" + s + "</li>" + "<br>"
-	string += "</ul>"
-	return string
+    # create variable to hold the list
+    string = "<ul><br>"
+    # loop through the list of strings and apply list tag to each element.
+    for s in strings:
+        string += "<li>" + s + "</li>" + "<br>"
+    string += "</ul>"
+    return string
 
 # calls list function and passes the user inputed list
 print(list(user_list))
